cad6a44c0fef80deaebdc6c2a917ae57084d8c00
Sharpnip <1430047@apps.bart.qc.ca>
2015-07-22 13:41:58 -0400

    Attempt to commit the whole project so we can this pool party going.

---
+++ 3DShip/3DShip/ShipRace.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Component.h"
+#include "Triangle.h"
+#include "Cube.h"
+#include "TriGrid.h"
+
+class ShipRace :
+	public Component
+{
+public:
+	ShipRace();
+	~ShipRace();
+
+	void Start();
+	void Update();
+	void Draw();
+	void Stop();
+
+private:
+	Triangle* triangle;
+	Cube* cube;
+	TriGrid* triGrid;
+};
+
