0b99457282ab92a6c00aa83d8475e0b2c56fef26
Sharpnip <1430047@apps.bart.qc.ca>
2015-07-23 11:32:01 -0400

    MiniUpdate

--- 3DShip/3DShip/Score.cpp
+++ 3DShip/3DShip/Score.cpp
@@ -6,10 +6,10 @@ Score::Score()
 	, currentScore(0.0f)
 {
 	D3DXFONT_DESC fontDesc;
-	fontDesc.Height = 80;
-	fontDesc.Width = 40;
+	fontDesc.Height = 30;
+	fontDesc.Width = 30;
 	fontDesc.MipLevels = 0;
-	fontDesc.Italic = true;
+	fontDesc.Italic = false;
 	fontDesc.CharSet = DEFAULT_CHARSET;
 	fontDesc.OutputPrecision = OUT_DEFAULT_PRECIS;
 	fontDesc.PitchAndFamily = DEFAULT_PITCH | FF_DONTCARE;
@@ -36,11 +36,11 @@ void Score::Draw()
 {
 	RECT fontRect;
 	::GetClientRect(gApp->GetMainWindow(), &fontRect);
-	mFont->DrawTextA(0, _T(scoreString), -1, &fontRect, DT_LEFT | DT_TOP | DT_SINGLELINE, D3DCOLOR_XRGB(255, 255, 255));
+	mFont->DrawTextA(0, _T(scoreString), -1, &fontRect, DT_LEFT | DT_BOTTOM | DT_SINGLELINE, D3DCOLOR_XRGB(255, 255, 255));
 }
 
 char* Score::GetScoreAsString(float deltaTime)
 {
-	scoreString = "Meow";
+	scoreString = "DEFAULT_SCORE";
 	return scoreString;
 }
