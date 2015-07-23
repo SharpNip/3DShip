7681c1e7df6c2ed549f24daad5b0c8431d542804
Sharpnip <1430047@apps.bart.qc.ca>
2015-07-23 10:48:47 -0400

    Updated Score class. Now draws to screen. Implementation of logic behind it will be done a little later.

--- 3DShip/3DShip/Score.h
+++ 3DShip/3DShip/Score.h
@@ -1,8 +1,23 @@
 #pragma once
+#include "Sprite.h"
+#include "Utils.h"
+#include <stdio.h>
+
 class Score
+	: public Component
 {
 public:
 	Score();
 	~Score();
+
+	void Update();
+	void Draw();
+	void IncrementScore(char* score);
+	char* GetScoreAsString(float deltaTime);
+
+private:
+	char* scoreString;
+	float currentScore;
+	ID3DXFont* mFont;
 };
 
