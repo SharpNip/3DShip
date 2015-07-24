e25847b83178d901871679ac61cda5f507239f34
Spardason <eric-vaillancourt@hotmail.ca>
2015-07-24 12:57:37 -0400

    Implemented SetScale in the Model

--- 3DShip/3DShip/Ship.h
+++ 3DShip/3DShip/Ship.h
@@ -1,10 +1,16 @@
 #pragma once
 #include "PrimitiveModel.h"
+
 class Ship :
 	public PrimitiveModel
 {
 public:
 	Ship();
 	~Ship();
+
+private:
+	void HandleInput();
+	void Move();
+	void OnCollision();
 };
 
