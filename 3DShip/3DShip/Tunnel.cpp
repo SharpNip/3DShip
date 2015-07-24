7f657f9cb35aac7b1e12f48dcbcba0c424f7f716
Max <1430070@P-15.college.bart.qc.ca>
2015-07-24 13:29:56 -0400

    Tunnel functionnal

--- 3DShip/3DShip/Tunnel.cpp
+++ 3DShip/3DShip/Tunnel.cpp
@@ -3,8 +3,16 @@
 
 Tunnel::Tunnel()
 	: PrimitiveModel(PrimitiveModel_Type::CYLINDER)
+	, mPos(0.f, 0.f, 0.f)
+	, SCALE_X(20.f)
+	, SCALE_Y(20.f)
+	, SCALE_Z(100.f)
+	, TUNNEL_SPEED(0.0001f)
+	// Leave this 0.9 so the tunnel doesn't flicker.
+	, RESET_POS(0.9f)
 {
-	
+	// Set the size of the tunnel.
+	SetScale(SCALE_X, SCALE_Y, SCALE_Z);
 }
 
 Tunnel::~Tunnel()
@@ -12,3 +20,21 @@ Tunnel::~Tunnel()
 	
 }
 
+void Tunnel::Update()
+{
+	// Move the tunnel with his z position.
+	MoveTunnel();
+}
+
+void Tunnel::MoveTunnel()
+{
+	// Update the new position of the tunnel.
+	mPos.z += TUNNEL_SPEED;
+	SetPosition(mPos.x, mPos.y, mPos.z + TUNNEL_SPEED);
+
+	// If the position reaches the reset pos, returns the tunnel to 0.
+	if (mPos.z >= RESET_POS)
+	{
+		mPos.z = 0;
+	}
+}
