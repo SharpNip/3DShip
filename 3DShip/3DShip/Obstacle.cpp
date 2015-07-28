65061a4885d273ae6de3274c60cc3a87546e26a4
Max <1430070@P-15.college.bart.qc.ca>
2015-07-28 12:14:56 -0400

    Yellow cube obstacles

--- 3DShip/3DShip/Obstacle.cpp
+++ 3DShip/3DShip/Obstacle.cpp
@@ -2,9 +2,11 @@
 
 Obstacle::Obstacle()
 	: PrimitiveModel(PrimitiveModel_Type::CUBE)
-	, posX(rand() % 10 - 5)
-	, posY(rand() % 10 - 5)
-	, depth(20)
+	, startingDepth(20)
+	, myRandom(rand() % 10 - 5)
+	, posX(myRandom)
+	, posY(myRandom)
+	, depth(startingDepth)
 	, OBSTACLE_SPEED(10)
 	, resetZ(-20)
 {
@@ -45,7 +47,7 @@ void Obstacle::MoveObstacle(float dt)
 void Obstacle::ResetPosition()
 {
 	// Reset with a new position.
-	posX = rand() % 10 - 5;
-	posY = rand() % 10 - 5;
-	depth = 20;
+	posX = myRandom;
+	posY = myRandom;
+	depth = startingDepth;
 }
--- 3DShip/3DShip/Transform.fx
+++ 3DShip/3DShip/Transform.fx
@@ -121,7 +121,7 @@ float4 TransformPS_Ship(OutputVS inVS) : COLOR0
 
 float4 TransformPS_Obstacle(OutputVS inVS) : COLOR0
 {
-	return float4(1.f, 1.f, 1.f, 1.f);
+	return float4(1.f, 1.f, 0.f, 1.f);
 }
  
 // Techniques
